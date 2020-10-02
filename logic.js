04ce0942d681b29272bdff9ff871aed37342def6
Elliott Payne <e.payne000@gmail.com>
2020-10-02 13:56:17 -0500

    added js search button fixed html links

--- logic.js
+++ logic.js
@@ -1,17 +1,12 @@
 $(document).ready(function(){
-
+    console.log("hi")
     var APIKey = "t7bAAaUxOAhdGnsJkTLVIFigHFNthOGu"
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=" + APIKey;
     
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        console.log(response);
-        
-    });
+    
 
     $("#searchBtn").on("click", function(){
+        console.log("hi2")
         $.ajax({
             url: queryURL,
             method: "GET"
