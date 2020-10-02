18a925257484a178aee7f3835a3d6644372e4915
Elliott Payne <e.payne000@gmail.com>
2020-10-02 13:32:49 -0500

    added some js

--- logic.js
+++ logic.js
@@ -1,17 +1,27 @@
 $(document).ready(function(){
 
     var APIKey = "t7bAAaUxOAhdGnsJkTLVIFigHFNthOGu"
-    var queryURL =
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=" + APIKey;
     
     $.ajax({
         url: queryURL,
         method: "GET"
-      }).then(function(response) {
+    }).then(function(response) {
+        console.log(response);
         
-        
-      });
+    });
 
+    $("#searchBtn").on("click", function(){
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+            
+        });
+    
 
+    })    
 
 
 
